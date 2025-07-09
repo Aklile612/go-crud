d926047244151e33e9ab753c0444867069c2f270
Aklile Ansa <aklilemente72@gmail.com>
2025-07-09 11:14:59 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/Aklile612/go-crud/initalizers"
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initalizers.LoadEnvVariables()
+}
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run() // Default runs on :8080
+}
