4f18a27430aa7e3c965474f701df1ca80c67c0eb
Aklile Ansa <aklilemente72@gmail.com>
2025-07-09 11:48:50 +0300

    edited

--- initalizers/datebase.go
+++ initalizers/datebase.go
@@ -1,13 +1,21 @@
 package initalizers
 
 import (
+	"log"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
+
 var DB *gorm.DB
+
 func ConnectionToDB() {
 	var err error
 	dsn := "host=localhost user=postgres password=aklile1996 dbname=go-crud port=5432 sslmode=disable"
 
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("Failed to connect to database")
+	}
 }
