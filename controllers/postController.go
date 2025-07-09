314a235502f1904517e2a37455f292a04c8cc100
Aklile Ansa <aklilemente72@gmail.com>
2025-07-09 13:34:41 +0300

    edited

--- controllers/postController.go
+++ controllers/postController.go
@@ -1,15 +1,27 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/Aklile612/go-crud/initalizers"
+	"github.com/Aklile612/go-crud/models"
+	"github.com/gin-gonic/gin"
+)
 
 func PostCreate(c *gin.Context) {
 
 	//get data from request body
 
 	//create a post
-	
+
+	post:=models.Post{Title: "hello", Body: "Post the body"}
+	result:= initalizers.DB.Create(&post)
+
+
+	if result.Error != nil{
+		c.Status(400)
+		return
+	}
 	//return it
 	c.JSON(200, gin.H{
-		"message": "pong",
+		"Post": post,
 	})
 }
