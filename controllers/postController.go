a8b60adbba2ec6a2a9e2449f0efe6049d2fc01f9
Aklile Ansa <aklilemente72@gmail.com>
2025-07-09 13:29:49 +0300

    edited

--- controllers/postController.go
+++ controllers/postController.go
@@ -3,6 +3,12 @@ package controllers
 import "github.com/gin-gonic/gin"
 
 func PostCreate(c *gin.Context) {
+
+	//get data from request body
+
+	//create a post
+	
+	//return it
 	c.JSON(200, gin.H{
 		"message": "pong",
 	})
